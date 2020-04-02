6ea22da8b7200e2942c8c61c49a37a6131a71cb1
Anthony Mejia <mejiaanthony016@gmail.com>
2020-04-02 16:31:27 -0600

    Anadimos algunos cambios

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+//Se instancia una nueva clase
 class Vehiculo{
     constructor(nombre, tipo){
         this.nombre = nombre;
     }
 }
-//declarando el objeto
+//Declaramos el objeto
 let Vehiculo1 = document.getElementById("vehiculo").value;
 const VehiculoUno = new Vehiculo(Vehiculo1, tipo1);
 //imprimiendo en consola
 
-//herencia
-class Serie extends Vehiculo{
+//Creacion de clase TipoVehiculo hereda de Vehiculo
+class TipoVehiculo extends Vehiculo{
     constructor(nombre, tipo, anio){
         super(nombre);
         this.tipo = tipo; 
@@ -17,6 +18,7 @@ class Serie extends Vehiculo{
     }
 }
 
+//Mostrando datos en pantalla
 let tipo = document.getElementById("tipo").value;
 let anio = document.getElementById("anio").value;
 alert("Es un vehiculo" + nombre + tipo + anio);
